532e675d3440d887e8e7573ef7bc28d06d7a376d
Ronadier <62849569+Ronadier@users.noreply.github.com>
2020-10-15 23:43:20 +0300

    Complete Task3hard

--- src/main/java/Task3hard.java
+++ src/main/java/Task3hard.java
@@ -1,4 +1,67 @@
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Task3hard {
+  public static void main(String[] args) throws IOException {
+
+    String str = getStr();
+    int maxAmount = getInt();
+    generateAndPrintJSON(str,maxAmount);
+  }
+
+  private static String getStr() throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String str = "";
+    while (str.length() == 0) {
+      System.out.println("Введите строку");
+      str = reader.readLine();
+      if (str.length() == 0) System.out.println("Вы ввели пустую строку, введите строку ещё раз");
+    }
+    return str;
+  }
+
+  private static int getInt() throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    int maxAmount = 0;
+    while (maxAmount <= 0) {
+      System.out.println("Введите число:");
+      maxAmount = Integer.parseInt(reader.readLine());
+      if (maxAmount <= 0 ) System.out.println("Вы ввели число меньше нуля или ноль. Введите число ещё раз");
+    }
+    return maxAmount;
+  }
+
+  private static Map<String, Integer> getWordsAndAmount(String str, int maxAmount){
+    String a = str.toLowerCase().trim();
+    String[] subStr;
+    subStr = a.split("(?U)\\W+"); //разбиваем на слова. W+ - регулярное выражение для всех знаков препинания, пробелов и пр., (?U) для поддержки юникода
+    int[] c = new int[subStr.length];
+    Map<String, Integer> result = new HashMap<String, Integer>();
+    for (int i =0;i<subStr.length;i++) {
+      for (int j=i;j<subStr.length;j++){
+       if (subStr[i].equals(subStr[j])) c[i]++;
+      }
+    }
+    for (int i=0;i<c.length;i++){
+      if (c[i]>maxAmount) result.put(subStr[i],c[i]);
+    }
+    return result;
+  }
+  private static void generateAndPrintJSON(String str, int maxAmount){
+    // convert Java to json
+    JSONObject root = new JSONObject(); // создаем главный объект JSON
+    HashMap<String, Integer> a = (HashMap<String, Integer>) getWordsAndAmount(str, maxAmount);
+    a.forEach((k, v) -> root.put(k,v)); //кладём мапу в JSON
+    String json = root.toString();
+    System.out.println(json);
+
+  }
 }
 
 /*
