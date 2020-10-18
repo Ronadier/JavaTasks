e7907d0e7c14637f7f35a6392068564a394c9f91
Ronadier <62849569+Ronadier@users.noreply.github.com>
2020-10-18 13:55:14 +0300

    Как бы это не было странно, но вроде бы решил 4хард

--- src/main/java/Task4.java
+++ src/main/java/Task4.java
@@ -3,7 +3,6 @@ import Helpers.InputHelper;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Task4 {
--- src/main/java/Task4hard.java
+++ src/main/java/Task4hard.java
@@ -3,25 +3,37 @@ import Helpers.InputHelper;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Task4hard {
   public static void main (String[] args) throws IOException {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     List<Integer> a = InputHelper.getIntList();
     int n = 0;
+    List<Integer> b = new ArrayList<>();
     System.out.println("Введите число N");
     n = Integer.parseInt(reader.readLine());
-    Set<Integer> b = new HashSet<>();
-    for (Integer integer : a) {
-      if (integer < n) b.add(integer);
-      else break;
+    for (int i = 0; i < a.size(); i++){ //Т.к. высчитать одно из слагаемых можно с помощью разности
+      Integer frequency = n - a.get(i); //то делаем лист с элементами сумма-одно из слагаемых (n-a)
+      b.add(frequency);
     }
-    b.forEach(System.out::println);
-    
+    //дальше - жесть :)
+    boolean isSumTrue = false;
+    for (int i = 0;i< a.size();i++){
+      if (b.contains(a.get(i)) && b.get(i) != a.get(i)) { //Если лист с разностью содержит элемент a и при этом они не находятся на одной позиции в листе
+        System.out.println("True"); //то всё ок
+        isSumTrue = true;
+        break;
+      } else{
+        b.set(i, null); //а тут обрабатываем ситуацию, когда мы вводим два одинаковых числа, но сумма их даёт искомый N
+        if (b.contains(a.get(i))) { //если одно из чисел обнуляем, но ещё одно такое же осталось - значит всё ок, а если нет - значит это просто случайно попалась сумма одного числа (например N=8, a=4)
+          System.out.println("True");
+          isSumTrue = true;
+          break;
+        }
+      }
+    }
+    if (!isSumTrue) System.out.println("False");
   }
 }
 
