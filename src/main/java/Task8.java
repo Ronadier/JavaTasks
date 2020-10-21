7f8ce3fe00f4bd2293ebbdb9b4dc8eb92484ea31
Ronadier <62849569+Ronadier@users.noreply.github.com>
2020-10-21 19:30:27 +0300

    t8light

--- src/main/java/Task8.java
+++ src/main/java/Task8.java
@@ -1,15 +1,28 @@
 import Helpers.InputHelper;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Task8 {
   public static void main (String[] args) throws IOException {
     System.out.println("Какое число переводим?");
     int a = InputHelper.getInt();
-    System.out.println("В какую систему счисления переводим?(2/8/16)");
-    int b = InputHelper.getInt();
-    if (b==2) System.out.println(Integer.toBinaryString(a));
-    else if (b==8) System.out.println(Integer.toHexString(a));
-    else if (b==16) System.out.println(Integer.toOctalString(a));
+    System.out.println(Integer.toBinaryString(a));
+    System.out.println(Integer.toHexString(a));
+    System.out.println(Integer.toOctalString(a));
+
+    dva(a);
+  }
+
+  private static void dva (int a){
+    ArrayList<Integer> c = new ArrayList<>();
+    do {
+      if (a%2 == 0) c.add(0);
+      else c.add(1);
+      a = a/2;
+    } while (a!=0);
+    for (int i = c.size()-1; i>=0;i--){
+      System.out.print(c.get(i));
+    }
   }
 }
