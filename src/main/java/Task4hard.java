9a2808ef75ff46d501606bb3108238b4aa2da415
Ronadier <62849569+Ronadier@users.noreply.github.com>
2020-10-26 13:13:28 +0300

    upd t4h

--- src/main/java/Task4hard.java
+++ src/main/java/Task4hard.java
@@ -6,25 +6,22 @@ import java.util.List;
 
 public class Task4hard {
   public static void main (String[] args) throws IOException {
-    List<Integer> a = InputHelper.getIntList();
-    int n = InputHelper.getInt();
-    if (isSumTrue(a,n)) System.out.println("True");
+    List<Integer> toDoList = InputHelper.getIntList();
+    int sumForFind = InputHelper.getInt();
+    if (isSumTrue(toDoList,sumForFind)) System.out.println("True");
     else System.out.println("False");
   }
 
-  private static boolean isSumTrue(List<Integer> a, int n){
-    List<Integer> b = new ArrayList<>();
-    for (Integer integer : a) { //Т.к. высчитать одно из слагаемых можно с помощью разности
-      Integer difference = n - integer; //то делаем лист с элементами сумма-одно из слагаемых (n-a)
-      b.add(difference);
-    }
-    //дальше - жесть :)
-    for (int i = 0;i< a.size();i++){
-      if (b.contains(a.get(i)) && !b.get(i).equals(a.get(i))) { //Если лист с разностью содержит элемент a и при этом они не находятся на одной позиции в листе
+  private static boolean isSumTrue(List<Integer> toDoList, int sumForFind){
+    List<Integer> differensList = new ArrayList<>();
+    for (int i = 0;i< toDoList.size();i++){
+      Integer difference = sumForFind - toDoList.get(i); //делаем лист с элементами сумма-одно из слагаемых (n-a)
+      differensList.add(difference);
+      if (differensList.contains(toDoList.get(i)) && !differensList.get(i).equals(toDoList.get(i))) { //Если лист с разностью содержит элемент a и при этом они не находятся на одной позиции в листе
         return true; //то всё ок
       } else{
-        b.set(i, null); //а тут обрабатываем ситуацию, когда мы вводим два одинаковых числа, но сумма их даёт искомый N
-        if (b.contains(a.get(i))) { //если одно из чисел обнуляем, но ещё одно такое же осталось - значит всё ок, а если нет - значит это просто случайно попалась сумма одного числа (например N=8, a=4)
+        differensList.set(i, null); //а тут обрабатываем ситуацию, когда мы вводим два одинаковых числа, но сумма их даёт искомый N
+        if (differensList.contains(toDoList.get(i))) { //если одно из чисел обнуляем, но ещё одно такое же осталось - значит всё ок, а если нет - значит это просто случайно попалась сумма одного числа (например N=8, a=4)
           return true;
         }
       }
