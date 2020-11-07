371d39d450c3be66804191690ccbe4837f426fe0
Ronadier <62849569+Ronadier@users.noreply.github.com>
2020-11-07 10:41:10 +0300

    написали число букавками

--- src/main/java/Task1Day17.java
+++ src/main/java/Task1Day17.java
@@ -6,33 +6,74 @@ import java.util.Locale;
 
 public class Task1Day17 {
   static final String[] BELOW_TWENTY = { "ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять", "одиннадцать", "двенадцадь", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать" };
+  static final String[] BELOW_TWENTY_F = {"ноль", "одна", "две"};
   static final String[] TENS = { "десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто" };
-  static final String[] HUNDREDS = { "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемсот", "девятьсот"};
+  static final String[] HUNDREDS = { "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемmсот", "девятьсот"};
   static final String[] THOUSANDS = {"тысяча", "тысячи", "тысяч"};
   static final String[] MILLIONS = {"миллион", "миллиона", "миллионов"};
   public static void main (String[] args) throws IOException {
     int startInt = InputHelper.getInt();
-    String startStr = String.valueOf(startInt);
-    char[] startChar = startStr.toCharArray();
-    RuleBasedNumberFormat nf = new RuleBasedNumberFormat(Locale.forLanguageTag("cz"), //готовая библиотека под это для проверки
+    RuleBasedNumberFormat nf = new RuleBasedNumberFormat(Locale.forLanguageTag("ru"), //готовая библиотека под это для проверки
             RuleBasedNumberFormat.SPELLOUT);
     System.out.println(nf.format(startInt));
 
 
     String text = "";
+    boolean isFemale = false;
     if (startInt < 1000){
-      text = fromOneToThousand(startInt);
+      text = fromOneToThousand(startInt, isFemale);
     } else if (startInt < 1000000){
-      text = fromOneToThousand(startInt/1000) + " " + THOUSANDS[0] + " " + fromOneToThousand(startInt%1000);
-    } else if (startInt < 1000000000)
-      text = fromOneToThousand(startInt/1000000) + " " + MILLIONS[0] + " " + fromOneToThousand(startInt/1000) + " " + THOUSANDS[0] + " " + fromOneToThousand(startInt%1000);
-    System.out.println(text);
-      //System.out.println(HUNDREDS[0]);
+      text = fromThousandToMillion(startInt, isFemale);
+    } else if (startInt < 1000000000) {
+      text = fromMillionToBillon(startInt, isFemale);
+    }
+      System.out.println(text);
   }
 
-  private static String fromOneToThousand(int startInt){
-    if ( startInt < 20 )
+  private static String fromMillionToBillon(int startInt, boolean isFemale){
+    String text = "";
+    int tens = (startInt/1000000)%100%10; //получаем разряд единиц (до десяти) для правильного склонения
+    switch (tens){
+      case 1: text = fromOneToThousand(startInt / 1000000, isFemale) + " " + MILLIONS[0];
+        break;
+      case 2:
+      case 3:
+      case 4:
+        text = fromOneToThousand(startInt / 1000000, isFemale) + " " + MILLIONS[1];
+        break;
+      default: text = fromOneToThousand(startInt / 1000000, isFemale) + " " + MILLIONS[2];
+    }
+    if (startInt % 1000000 > 1000){
+      text +=  " " + fromThousandToMillion(startInt % 1000000, isFemale);
+    } else if (startInt % 1000000 != 0){
+      text +=  " " + fromOneToThousand(startInt % 1000000, isFemale);
+    }
+    return text;
+  }
+
+  private static String fromThousandToMillion(int startInt, boolean isFemale){
+    String text = "";
+    int tens = (startInt/1000)%100%10; //получаем разряд единиц (до десяти) для правильного склонения
+    switch (tens){
+      case 1 : text = fromOneToThousand(startInt/1000, isFemale=true) + " " + THOUSANDS[0];
+        break;
+      case 2:
+      case 3:
+      case 4:
+        text = fromOneToThousand(startInt/1000, isFemale) + " " + THOUSANDS[1];
+        break;
+      default: text = fromOneToThousand(startInt/1000, isFemale) + " " + THOUSANDS[2];
+    }
+    if (startInt % 1000 !=0) {
+      text += " " + fromOneToThousand(startInt % 1000, isFemale);
+    }
+    return text;
+  }
+
+  private static String fromOneToThousand(int startInt, boolean isFemale){
+    if ( startInt < 20 && !isFemale) {
       return BELOW_TWENTY[startInt];
+    } else if (startInt < 3 && isFemale) return BELOW_TWENTY_F[startInt];
     else if ( startInt < 100 ) {
       return lowHundert(startInt);
     }
@@ -53,10 +94,17 @@ public class Task1Day17 {
 
   private static String lowThousand (int startInt){
     int high = startInt / 100;
-    int mid = startInt % 10;
-    mid = mid/10;
+    int mid = startInt % 100;
+    String text = "";
+    if (mid == 0) return HUNDREDS[high-1];
+    if (mid <= 20){
+      text = HUNDREDS[high - 1] + " " + BELOW_TWENTY[mid];
+      return text;
+    } else {
+      mid = mid/10;
+      text = HUNDREDS[high - 1] + " " + TENS[mid-1];
+    }
     int low = startInt % 10;
-    String text = HUNDREDS[high - 1] + " " + TENS[mid];
     if ( low != 0 )
       text = text + " " + BELOW_TWENTY[low];
     return text;
