885ed51c8f38a42161eb70986cfe32b3525a86b1
Ronadier <62849569+Ronadier@users.noreply.github.com>
2020-11-08 12:31:01 +0300

    запилил проверку при инициализации

--- src/main/java/Task1Day19.java
+++ src/main/java/Task1Day19.java
@@ -4,10 +4,13 @@ import java.util.Arrays;
 public class Task1Day19 {
   public static void main(String[] args) {
     int[] start = new int[100];
+    boolean[] isContains = {false, false};
     for (int i=0;i<start.length;i++){
       start[i] = (int)(Math.random()*100);
+      if (start[i] == 13) isContains[0] = true;
+      else if (start[i] == 41) isContains[1] = true;
     }
-    if (Arrays.asList(start).contains(13) && Arrays.asList(start).contains(41)) System.out.println("True");
+    if (isContains[0] == true && isContains[1] == true) System.out.println("True");
     else System.out.println("False");
   }
 }
